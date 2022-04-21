2e9bd2cbb797aa2a099e0a61115d684b850fc1c5
Luiza-Arantes <luiza.acc10@gmail.com>
2022-04-21 18:28:07 -0300

    Base animação e partícula

--- Worshop Arte/Assets/Scripts/AnimationController.cs
+++ Worshop Arte/Assets/Scripts/AnimationController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
-    private Animator animator; 
+    private Animator animator;
+    private ParticleSystem ps;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        ps = GetComponent<ParticleSystem>();
+
     }
 
     public void TriggerAnimation()
